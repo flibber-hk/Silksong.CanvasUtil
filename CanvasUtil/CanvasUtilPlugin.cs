9bd2a846ea5652583884b5e5d68162a120a9dd97
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2025-11-08 14:25:29 +0000

    Add warning disable

--- CanvasUtil/CanvasUtilPlugin.cs
+++ CanvasUtil/CanvasUtilPlugin.cs
@@ -3,7 +3,9 @@ using BepInEx;
 namespace CanvasUtil;
 
 [BepInAutoPlugin(id: "io.github.flibber-hk.canvasutil")]
+#pragma warning disable CS1591  // Missing XML comment for publicly visible type or member
 public partial class CanvasUtilPlugin : BaseUnityPlugin
+#pragma warning restore CS1591  // Missing XML comment for publicly visible type or member
 {
     private void Awake()
     {
