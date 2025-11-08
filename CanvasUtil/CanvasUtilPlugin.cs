96761c794ad591c7f284e3ce597e3e3993b1a622
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2025-11-08 08:34:50 +0000

    Add code

--- CanvasUtil/CanvasUtilPlugin.cs
+++ CanvasUtil/CanvasUtilPlugin.cs
@@ -2,13 +2,11 @@ using BepInEx;
 
 namespace CanvasUtil;
 
-// TODO - adjust the plugin guid as needed
 [BepInAutoPlugin(id: "io.github.flibber-hk.canvasutil")]
 public partial class CanvasUtilPlugin : BaseUnityPlugin
 {
     private void Awake()
     {
-        // Put your initialization logic here
         Logger.LogInfo($"Plugin {Name} ({Id}) has loaded!");
     }
 }
