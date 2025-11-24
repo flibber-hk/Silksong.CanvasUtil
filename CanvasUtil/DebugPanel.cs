2d1dfcf4daa9a14ff5b3edf4e42c33167e304228
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2025-11-24 06:49:38 +0000

    Add DebugPanel

---
+++ CanvasUtil/DebugPanel.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CanvasUtil;
+
+/// <summary>
+/// Class with utilities for creating a text panel to show debug info.
+/// 
+/// This is useful for debugging aspects of your mod,
+/// and is not expected to be used in released mods.
+/// </summary>
+public class DebugPanel : MonoBehaviour
+{
+    private const int FONT_SIZE = 18;
+
+    /// <summary>
+    /// Create a panel showing a single string that can update each frame.
+    /// </summary>
+    /// <param name="generator">Function to generate the text that will be shown each frame.
+    /// The text can spread over multiple lines.
+    /// 
+    /// If this is null, the text will not automatically update and should be manually
+    /// updated by calling <see cref="SetText"/>.
+    /// </param>
+    public static DebugPanel Create(Func<string>? generator = null)
+    {
+        GameObject canvas = CanvasUtil.CreateCanvas(RenderMode.ScreenSpaceOverlay, new Vector2(1920, 1080));
+        canvas.name = $"{nameof(DebugPanel)} Canvas";
+        
+        CanvasGroup cg = canvas.GetComponent<CanvasGroup>();
+        cg.interactable = false;
+        cg.blocksRaycasts = false;
+        
+        DontDestroyOnLoad(canvas);
+
+        GameObject basePanel = CanvasUtil.CreateBasePanel(
+            canvas,
+                new RectData(
+                new(550, 700),
+                new(100, -300),
+                new(0, 1),
+                new(0, 1),
+                new(0, 1)
+                )
+            );
+        Image img = basePanel.AddComponent<Image>();
+        img.color = new Color(0, 0, 0, 0.25f);
+
+        GameObject textPanel = CanvasUtil.CreateTextPanel(
+            basePanel, string.Empty, fontSize: FONT_SIZE, textAnchor: TextAnchor.UpperLeft,
+            new RectData(new(500, 650), new(25, -25)),
+            DefaultFont
+            );
+
+        if (generator is not null)
+        {
+            TextUpdate tu = textPanel.AddComponent<TextUpdate>();
+            tu.Generator = generator;
+        }
+
+        DebugPanel debugPanel = canvas.AddComponent<DebugPanel>();
+        debugPanel.Text = textPanel.GetComponent<Text>();
+        debugPanel.basePanel = basePanel;
+
+        return debugPanel;
+    }
+
+    /// <summary>
+    /// Set the text that is being displayed.
+    /// </summary>
+    /// <param name="s">The text that will be shown. This can spread over multiple lines.</param>
+    public void SetText(string s)
+    {
+        Text.text = s;
+    }
+
+    /// <summary>
+    /// The <see cref="UnityEngine.UI.Text"/> object that is displaying the text.
+    /// </summary>
+    public Text Text { get; private set; } = null!;
+
+    /// <summary>
+    /// Toggle whether the panel is visible.
+    /// </summary>
+    public void SetPanelVisible(bool visible)
+    {
+        basePanel?.SetActive(visible);
+    }
+
+    private GameObject basePanel = null!;
+
+    private static readonly string[] OSFonts =
+    {
+        "Consolas",
+        "Menlo",
+        "Courier New",
+        "DejaVu Mono"
+    };
+    private static Font? _font;
+
+    /// <summary>
+    /// The default font used for the debug panel.
+    /// </summary>
+    public static Font DefaultFont
+    {
+        get
+        {
+            if (_font == null)
+            {
+                _font = Font.CreateDynamicFontFromOSFont(OSFonts, FONT_SIZE);
+            }
+            return _font;
+        }
+    }
+
+    [RequireComponent(typeof(Text))]
+    private class TextUpdate : MonoBehaviour
+    {
+        public Func<string>? Generator { get; set; }
+        private Text _text;
+
+        void Awake() => _text = GetComponent<Text>();
+
+        void Update() => _text.text = Generator?.Invoke() ?? string.Empty;
+    }
+}
--- README.md
+++ README.md
@@ -21,3 +21,42 @@ as with the parent.
 For example, to put a child in the bottom-right corner of the parent, you can set the anchor pivot, anchor min and anchor max to (1, 0) - this
 represents the bottom right corner (max x, min y). The anchor position should be (0, 0) to represent the fact that the anchor pivot
 (bottom right corner of parent) and anchor (bottom right corner of child) are the same point.
+
+
+## DebugPanel
+
+A utility added to help in mod development. Sometimes you want to know information that may change
+each frame, so logging is probably not the most helpful way to do it. CanvasUtil provides a utility
+called DebugPanel to help with this.
+
+The easiest way to do this is to simply do the following:
+
+```
+// In your plugin class, define a function to get the text to display
+private static string GetDebugPanelText()
+{
+    // Code goes here. This function will be called each frame
+    // to figure out what the text to display should be.
+}
+
+// In your plugin Awake method
+CanvasUtil.DebugPanel.Create(GetDebugPanelText);
+```
+
+An alternative approach can be as follows:
+```
+// In your plugin class, define a function to update the text and an event raised when updating the text
+private static event System.Action<string>? OnUpdateDebugPanelText;
+internal static void UpdateDebugPanelText(string newText) => OnUpdateDebugPanelText?.Invoke(newText);
+
+// In your plugin Awake method
+var dp = CanvasUtil.DebugPanel.Create();
+OnUpdateDebugPanelText += dp.SetText;
+
+// Anywhere else in your mod
+MyPlugin.UpdateDebugPanelText("the new text to display");
+```
+
+This is designed to be used while developing mods and is not expected to be used in published versions of mods.
+In both of the example codes, simply deleting the code from the "In your plugin Awake method" section
+will stop the debug panel from being created without breaking any other aspect of your mod.
